0fe0b7c10e13e05715a7d65f2447867a645d5732
Alison Emilien <alison.emilien43@gmail.com>
2023-10-06 07:44:06 -0400

    Added Perl document

---
+++ Perl/HelloWorld.pl
@@ -0,0 +1,15 @@
+print "Hello World";
+printf("%s", "Hello World");
+
+#This is a one line comment
+
+=Description of this code
+this
+is a 
+multiline 
+comment
+=cut
+
+
+
+
---
+++ Perl/Variable.pl
@@ -0,0 +1,20 @@
+=POD - This is the first paragraph
+$string = "This is a string.";   # stores string
+$int = 5;     # stores an integer
+$float = 5.7;    # stores a floating point type value
+$char = 'a';    # stores character type value
+
+print $string, "\n";
+print "An integer type: ", $int, "\n";
+print "A float type: ", $float, "\n";
+print "A character type: ", $char, "\n";
+=cut
+
+
+=POD
+print "Hello World!\n";    # \n inserts a new line
+print "Hello\tWorld!\n";    # \t adds a tab space 
+print "\"Double quotes\"\n";  # use \ to add double quotes
+print '\'Single quotes\'';  # use \ to add single quotes
+=cut
+
